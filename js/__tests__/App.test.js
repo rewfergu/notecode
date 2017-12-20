ef6109463197db629226f18559d788d893191185
Robert Ferguson <rob@rewferguson.com>
2017-12-19 23:32:10 -0600

    replace node-sass for new version of node

---
+++ js/__tests__/App.test.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { mount } from "enzyme";
+
+import App from "../App";
+
+test.skip('create note', () => {});
+test.skip('select note', () => {});
+test.skip('save note', () => {});
+test.skip('delete note', () => {});
+test.skip('update title', () => {});
+test.skip('add tag', () => {});
+test.skip('delete tag', () => {});
+test.skip('update content', () => {});
+test.skip('update mode', () => {});
+test.skip('toggle sidebar', () => {});
+test.skip('search notes', () => {});
